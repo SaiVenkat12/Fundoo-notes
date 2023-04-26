79efc93f4f00a27306d861f32ca84ddc43c5958a
SaiVenkat12 <gsaiv1001@gmail.com>
2023-04-26 11:01:05 +0530

    Added- reset page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:LoginComponent
+  },
+  {
+    path:'signup',
+    component:SignupComponent,
+  },
+  {
+    path:'forgot-password',
+    component:ForgotPasswordComponent
+  },
+  {
+    path:'reset-password',
+    component:ResetPasswordComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
